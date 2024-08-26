78f7c802cae4df05d63c04f2821e5ae9bfbe4983
WebCodeX_ <144920361+WebCodeXorg@users.noreply.github.com>
2024-08-26 08:10:22 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,11 @@
+const container = document.getElementById('container');
+const login_btn = document.getElementById('login');
+const register_btn = document.getElementById('register');
+
+register_btn.addEventListener('click', () => {
+    container.classList.add('active');
+});
+
+login_btn.addEventListener('click', () => {
+    container.classList.remove('active');
+})
